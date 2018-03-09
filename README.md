51488b52fc444cc0fed4e7111726a8dc1384159d
thatbrguy <bharathrajn98@gmail.com>
2018-03-09 18:08:31 +0530

    Added Files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# thatbrguy.github.io
+# Portfolio
+
+Here's a webpage that I created which describes a little bit about me. [Link](https://thatbrguy.github.io).
---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    });
+});

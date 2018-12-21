9a24c74c70d2b23ce80eab19cd3f04955426ab1a
thatbrguy <bharathrajn98@gmail.com>
2018-12-21 23:53:58 +0530

    Updates

---
+++ precache-manifest.7f2336024354f28c290855b33caacdd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "366dd79b197437ecd641bb5ecb7c2306",
+    "url": "/static/media/Resume Nov 2018 v2.366dd79b.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bde22c253e8d709d69aa",
+    "url": "/static/js/main.bde22c25.chunk.js"
+  },
+  {
+    "revision": "34463df631e44aa8f3c9",
+    "url": "/static/js/1.34463df6.chunk.js"
+  },
+  {
+    "revision": "bde22c253e8d709d69aa",
+    "url": "/static/css/main.5c20c1a9.chunk.css"
+  },
+  {
+    "revision": "1ca788fd27877ffeecf4d2592882420d",
+    "url": "/index.html"
+  }
+];
